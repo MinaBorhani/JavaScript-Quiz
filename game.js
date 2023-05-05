00481fa80560e98038320c1ad16ca0fc43c8df61
Mina Borhani <92939258+MinaBorhani@users.noreply.github.com>
2023-05-05 02:12:28 -0700

    new

---
+++ game.js
@@ -0,0 +1,39 @@
+// console.log("hiii");
+const question = document.getElementById("question");
+const choice =Array.from(document.getElementsByName("choice-text"));
+// console.log(choice);
+
+let currentQuestion = {};
+let acceptingAnswers = true;
+let score = 0; 
+let questionCounter = 0;
+let availableQuestion = [];
+
+
+
+let questions = [
+    {
+        question : 'Inside wich HTML element do we put the JavavScript ?',
+        choice1:"<script>",
+        choice2:"<javascropt>",
+        choice3:"<js>",
+        choice4:"<scripting>",
+        answer:1
+    },
+    {
+        question : "what is the corrct syntax for referring to an external script called 'xxx.js'?",
+        choice1:"<script href='xxx.js'>",
+        choice2:"<script nmae='xxx.js'>",
+        choice3:"<script src='xxx.js'>",
+        choice4:"<script file='xxx.js'>",
+        answer:3
+    },
+    {
+        question : "How do you write 'Hello World' in an alert box",
+        choice1:"msgBox('Hello World');",
+        choice2:"alertBox('Hello World');",
+        choice3:"msg('Hello World');",
+        choice4:"alret('Hello World');",
+        answer:4
+    }
+]
