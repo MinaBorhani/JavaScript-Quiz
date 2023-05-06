d1b1cb95f88ad347f55a283a8b12e9b65970a3e6
Mina Borhani <92939258+MinaBorhani@users.noreply.github.com>
2023-05-05 21:00:03 -0700

    new

--- end.js
+++ end.js
@@ -4,6 +4,9 @@ const finalScore = document.getElementById("finalScore");
 const mostRecntScore = localStorage.getItem("mostRecntScore");
 finalScore.innerText = mostRecntScore;
 
+const highScore=JSON.parse(localStorage.getItem("highScore")) || [];
+console.log(highScore);
+
 username.addEventListener("keyup" , ()=>{
     // console.log(username.value);
     saveHightScore.disabled = !username.value;
@@ -12,4 +15,17 @@ username.addEventListener("keyup" , ()=>{
 saveHighScore = e =>{
     console.log('click button');
     e.preventDefault();
+    
+    const score = {
+        name : username.value,
+        // score : mostRecntScore
+        score : Math.floor(Math.random() * 100)
+    };
+    highScore.push(score)
+    highScore.sort((a,b) => b.score - a.score );
+    highScore.splice(5);
+
+    localStorage.setItem("highScore" , JSON.stringify(highScore))
+    window.location.assign("/");
+    // console.log(highScore);
 }
