ffc820fbbbca9bfcde20462897ba897c81ad9bf3
Mina Borhani <92939258+MinaBorhani@users.noreply.github.com>
2023-05-06 16:35:07 -0700

    new

--- game.js
+++ game.js
@@ -15,32 +15,19 @@ let availableQuestion = [];
 
 
 
-let questions = [
-    {
-        question : 'Inside wich HTML element do we put the JavavScript ?',
-        choice1:"<script>",
-        choice2:"<javascropt>",
-        choice3:"<js>",
-        choice4:"<scripting>",
-        answer:1
-    },
-    {
-        question : "what is the corrct syntax for referring to an external script called 'xxx.js'?",
-        choice1:"<script href='xxx.js'>",
-        choice2:"<script nmae='xxx.js'>",
-        choice3:"<script src='xxx.js'>",
-        choice4:"<script file='xxx.js'>",
-        answer:3
-    },
-    {
-        question : "How do you write 'Hello World' in an alert box",
-        choice1:"msgBox('Hello World');",
-        choice2:"alertBox('Hello World');",
-        choice3:"msg('Hello World');",
-        choice4:"alret('Hello World');",
-        answer:4
-    }
-]
+let questions = [];
+
+fetch("question.json").then(res =>{
+    // console.log(res.json());
+    return res.json()
+})
+.then(loadQuestion =>{
+    console.log(loadQuestion);
+    questions = loadQuestion;
+    straGame();
+}).catch( err =>{
+    console.error(err);
+})
 
 const CORRECT_BONUS = 10;
 const MAX_QUESTIONS = 3;
@@ -114,4 +101,3 @@ incrementScore = num =>{
     scoretext.innerText = score;
 }
 
-straGame();
